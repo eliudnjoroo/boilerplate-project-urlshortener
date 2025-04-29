b899a434608cf0a414d6cbede1f145908236339d
Eliud <124616502+eliudnjoroo@users.noreply.github.com>
2025-04-29 21:00:23 +0300

    init

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
+const dns = require('dns');
+const urlDatabase = {};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +21,42 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.use(urlencoded({extended: true}));
+app.use(express.json());
+
+app.post("/api/shorturl",(req, res)=>{
+  console.log(req.headers['content-type']);
+  console.log(req.body);
+
+  const inputUrl = req.body.url;
+
+  try {
+    const urlObj = new URL(inputUrl);
+    dns.lookup(urlObj.hostname, (err) => {
+      if (err) {
+        return res.json({ error: "invalid url" });
+      }
+      const short = Math.floor(Math.random() * 100000);
+      urlDatabase[short] = inputUrl;
+      res.json({ original_url: inputUrl, short_url: short });
+    });
+  } catch (e) {
+    return res.json({ error: "invalid url" });
+  }
+});
+
+app.get('/api/shorturl/:short', (req, res) => {
+  const short = req.params.short;
+  const originalUrl = urlDatabase[short];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: "No short URL found for given input" });
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
